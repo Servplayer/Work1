b5a9193090208f9c97bb5a7bf48ab15b4ae575ac
Servplayer <tank_3g@ukr.net>
2019-06-10 06:28:34 +0300

    Add files via upload

--- Work1/Work1/Program.cs
+++ Work1/Work1/Program.cs
@@ -17,29 +17,49 @@ namespace Work1
             Console.WriteLine("Введите максимальное значение х");
             max = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Введите шаг");
-            ch = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("y = x^2");
-            while(x<=max){
-                y=Math.Pow(x,2);
-                Console.WriteLine("y = " + y);
-                
-                x = x + ch;
-                y = 0;
-            }
-            x=min;
+            ch = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("function(x1, x2); x2=3*x1");
             while(x<=max)
             {
-                //y = (Math.Pow((Math.Sin(x)), 3)+45+3*x)/(2*Math.Pow(x,4)+4*3*x); //func of 20
-                y=Math.Sqrt((Math.Pow(3*x,2)+x/(3*x))/(16*3*x*x));//var 26
-                Console.WriteLine("y = " + y);
-                sum = y + sum;
+                y=Math.Sin(x - Math.Pow(3*x,3)+Math.Sqrt(x))-1.3*Math.Pow(x,3);
+                Console.WriteLine("f(x) = " + y);
+                sum = Math.Sin(y) + sum;
                 dob = dob * y;
                 x = x + ch;
                 y = 0;
             }
 
-            Console.WriteLine("Сумма промежуточных у = " + sum);
+            Console.WriteLine("Сумма синусов промежуточных значений f(x) = " + sum);
+            //part 2
+            Console.WriteLine("PART 2");
+            double min2, max2, x2;
+            sum = 0;
+            Console.WriteLine("Введите минимальные значение х1, x2");
+            min = Convert.ToDouble(Console.ReadLine());
+            min2 = Convert.ToDouble(Console.ReadLine());
+            x = min;
+            x2 = min2;
+            Console.WriteLine("Введите максимальные значение х1, x2");
+            max = Convert.ToDouble(Console.ReadLine());
+            max2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите шаг");
+            ch = Convert.ToDouble(Console.ReadLine());
+            while (x <= max)
+            {
+                while (x2 <= max2)
+                {
+                    y = Math.Sin(x - Math.Pow(x2, 3) + Math.Sqrt(x)) - 1.3 * Math.Pow(x, 3);
+                    Console.WriteLine("f(x) = " + y);
+                    if(y>0) sum = Math.Sin(y) + sum;
+                    dob = dob * y;
+                    x2 = x2 + ch;
+                    y = 0;
+                }
+                x = x + ch;
+                x2 = min;
+            }
+
+            Console.WriteLine("Сумма синусов промежуточных неотрицательных значений f(x) = " + sum);
             Console.ReadKey();
         }
     }
